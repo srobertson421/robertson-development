221fed4391a9347e75ea030fec49098d283419f1
Sean Robertson <srobertson421@gmail.com>
2017-03-28 15:47:50 -0700

    added basename to router for testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar/Navbar';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/robertson-development">
         <div>
           <Navbar />
           <Route exact path="/" component={Home} />
